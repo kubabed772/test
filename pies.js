318cdc19d386f44fef13841614538e2e7a474b70
Zydson123 <mlgplaygaming@gmail.com>
2023-12-13 10:50:38 +0100

    test

--- pies.js
+++ pies.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
+const { write } = require('fs');
 const XLSX = require('xlsx');
 
-// Funkcja pobierająca informacje o pushach z GitHub API
 async function getGitHubPushes(username, repository) {
     try {
         const response = await axios.get(`https://api.github.com/repos/${username}/${repository}/events`);
@@ -11,24 +11,64 @@ async function getGitHubPushes(username, repository) {
     }
 }
 
-// Funkcja zapisująca informacje do pliku XLSX
 function writeToXLSX(data) {
     const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.json_to_sheet(data);
+    const worksheetData = Array.isArray(data) ? data : [data]; 
+    const worksheet = XLSX.utils.json_to_sheet(worksheetData);
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Pushes');
     XLSX.writeFile(workbook, 'github_pushes.xlsx');
 }
 
-// Użycie funkcji do pobrania informacji o pushach i zapisania ich do pliku XLSX
+function countRepositoryStats(pushes) {
+    let whoPushed;
+    let pullRequestsCount = 0;
+    let reviewCommentsCount = 0;
+    let masterCommitsCount = 0;
+    let changedFilesCount = 0;
+
+    for (let i = 0; i < pushes.length; i++) {
+        const push = pushes[i];
+        whoPushed = push.actor.login
+        if (push.type === 'PushEvent' && push.payload && push.payload.ref === 'refs/heads/main') {
+            masterCommitsCount++;
+            if (push.payload && push.payload.commits) {
+                for (let j = 0; j < push.payload.commits.length; j++) {
+                    const commit = push.payload.commits[j];
+                    console.log('Additions:', commit.additions);
+                    console.log('Deletions:', commit.deletions);
+                    changedFilesCount += (commit.additions || 0) + (commit.deletions || 0);
+                }
+            }
+        } else if (push.type === 'PullRequestEvent') {
+            pullRequestsCount++;
+        } else if (push.type === 'PullRequestReviewCommentEvent') {
+            reviewCommentsCount++;
+        }
+    }
+
+    return {
+        who: whoPushed,
+        pullRequests: pullRequestsCount,
+        reviewComments: reviewCommentsCount,
+        masterCommits: masterCommitsCount,
+        changedFiles: changedFilesCount
+    };
+}
+
 const githubUsername = 'kubabed772';
 const githubRepository = 'test';
+const githubToken = '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'; 
 
-getGitHubPushes(githubUsername, githubRepository)
+getGitHubPushes(githubUsername, githubRepository, githubToken)
     .then(pushes => {
-        // Przetwarzanie informacji o pushach, jeśli to konieczne
-        console.log('Pobrano informacje o pushach:', pushes);
-        writeToXLSX(pushes);
-        console.log('Informacje zapisano do pliku github_pushes.xlsx');
+        const stats = countRepositoryStats(pushes);
+        console.log('Statystyki repozytorium:');
+        console.log('Kto zpushował: ', stats.who);
+        console.log('Liczba PRów:', stats.pullRequests);
+        console.log('Liczba komentarzy od reviewera:', stats.reviewComments);
+        console.log('Liczba commitów do mastera:', stats.masterCommits);
+        console.log('Liczba zmienionych plików:', stats.changedFiles);
+        writeToXLSX(stats)
     })
     .catch(error => {
         console.error(error.message);
